5051650f7e4ffc6602a1846cf893ba0669ae6dd6
IHaxU <30863082+IHaxU@users.noreply.github.com>
2025-08-19 19:47:53 +0200

    Update work.ink-bypass.user.js

--- work.ink-bypass.user.js
+++ work.ink-bypass.user.js
@@ -6,9 +6,9 @@
 // @author       IHaxU
 // @match        https://work.ink/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=work.ink
-// @run-at       document-start
 // @downloadURL  https://github.com/IHaxU/work.ink-bypass/raw/refs/heads/main/work.ink-bypass.user.js
 // @updateURL    https://github.com/IHaxU/work.ink-bypass/raw/refs/heads/main/work.ink-bypass.user.js
+// @run-at       document-start
 // ==/UserScript==
 
 (function() {
